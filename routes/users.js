bc94f0d6c5e155a3dc9eb3a8f3c6918d7fcd0877
Kacper W <wilczynski.kacper98@gmail.com>
2023-12-29 21:53:20 +0100

    added router.route routing

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ const User = require('../models/user');
 const users = require('../controllers/users')
 const { storeReturnTo } = require('../middleware');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin)  
-
-router.post('/login', storeReturnTo ,passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+router.route('/login')
+.get(users.renderLogin)  
+.post(storeReturnTo ,passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login);
 
 router.get('/logout', users.logout); 
 module.exports = router;
